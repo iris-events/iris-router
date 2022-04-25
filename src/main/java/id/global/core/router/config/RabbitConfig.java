80e6db94b5b65604a2c66bedda66e5c52901e9e9
Tomaz Cerar <tomaz.cerar@gmail.com>
2022-04-25 23:40:32 +0200

    bit diffrent way of config

--- src/main/java/id/global/core/router/config/RabbitConfig.java
+++ src/main/java/id/global/core/router/config/RabbitConfig.java
@@ -1,14 +1,17 @@
 package id.global.core.router.config;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.reactive.messaging.spi.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.quarkus.arc.DefaultBean;
+import io.quarkus.arc.profile.IfBuildProfile;
+import io.quarkus.arc.profile.UnlessBuildProfile;
 import io.smallrye.reactive.messaging.rabbitmq.RabbitMQConnector;
 import io.vertx.rabbitmq.RabbitMQClient;
 import io.vertx.rabbitmq.RabbitMQOptions;
@@ -27,22 +30,57 @@ public class RabbitConfig {
      */
 
     @Produces
-    public RabbitMQClient createClient() {
-        var options = createOptions();
-        log.info("options: host: {}, port: {}, ssl:{}", options.getHost(),options.getPort(), options.isSsl());
+    public RabbitMQClient createClient(RabbitMQOptions options) {
+        //var options = createOptions();
+        //log.info("options: host: {}, port: {}, ssl:{}", options.getHost(), options.getPort(), options.isSsl());
         return RabbitMQClient.create(connnector.getVertx().getDelegate(), options);
 
     }
 
     @Produces
-    //@Identifier("iris")
-    @Default
-    public RabbitMQOptions createOptions() {
-        return new RabbitMQOptions().setHost(System.getProperty("rabbitmq-host", "localhost"))
-                .setPort(Integer.parseInt(System.getProperty("rabbitmq-port", "5672")))
-                .setUser(System.getProperty("rabbitmq-username", "guest"))
-                .setPassword(System.getProperty("rabbitmq-password", "guest"))
-                .setSsl(Boolean.getBoolean("rabbitmq-ssl"));
+    @DefaultBean
+    @UnlessBuildProfile("prod")
+    @ApplicationScoped
+    public RabbitMQOptions createDevOptions(Config config) {
+        var host = config.getValue("rabbitmq-host", String.class);
+        var port = config.getValue("rabbitmq-port", Integer.class);
+        var username = config.getValue("rabbitmq-username", String.class);
+        var password = config.getValue("rabbitmq-password", String.class);
+        var ssl = config.getOptionalValue("rabbitmq-ssl", Boolean.class).orElse(false);
+        log.info("host: {}, port: {}, user: {}, pass: {}, ssl: {}", host, port, username, password, ssl);
+
+        var options = new RabbitMQOptions()
+                .setHost(host)
+                .setPort(port)
+                .setUser(username)
+                .setPassword(password)
+                .setSsl(ssl);
+        log.info("non prod options: host: {}, port: {}, ssl: {}", options.getHost(), options.getPort(), options.isSsl());
+        return options;
+
+    }
+
+    @Produces
+    @DefaultBean
+    @IfBuildProfile("prod")
+    @ApplicationScoped
+    public RabbitMQOptions createProdOptions(Config config) {
+
+        //log.info("config: {}", config.getPropertyNames());
+        log.info("need to configure prod");
+        var host = config.getValue("rabbitmq-host", String.class);
+        var port = config.getValue("rabbitmq-port", Integer.class);
+        var username = config.getValue("rabbitmq-username", String.class);
+        var password = config.getValue("rabbitmq-password", String.class);
+        var ssl = config.getOptionalValue("rabbitmq-ssl", Boolean.class).orElse(false);
+        log.info("host: {}, port: {}, user: {}, pass: {}, ssl: {}", host, port, username, password, ssl);
+
+        return new RabbitMQOptions()
+                .setHost(host)
+                .setPort(port)
+                .setUser(username)
+                .setPassword(password)
+                .setSsl(ssl);
 
     }
 }
