20f90373bf02de7fcc8fd0f045714f3e89a7fc75
Nejc Tomažič <nejc.tomazic@global.id>
2022-05-10 10:21:58 +0200

    INFRA-1576: update failing tests

--- src/test/java/id/global/core/router/ws/message/handler/DefaultMessageHandlerTest.java
+++ src/test/java/id/global/core/router/ws/message/handler/DefaultMessageHandlerTest.java
@@ -2,6 +2,7 @@ package id.global.core.router.ws.message.handler;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.UUID;
 
@@ -9,14 +10,22 @@ import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import id.global.core.router.model.AmqpMessage;
 import id.global.core.router.model.RequestWrapper;
 import id.global.core.router.model.UserSession;
 import id.global.core.router.service.BackendService;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
+import io.vertx.core.buffer.Buffer;
 
 @QuarkusTest
 class DefaultMessageHandlerTest {
+
+    @Inject
+    ObjectMapper objectMapper;
 
     @Inject
     @DefaultHandler
@@ -26,12 +35,16 @@ class DefaultMessageHandlerTest {
     BackendService backendService;
 
     @Test
-    void handle() {
+    void handle() throws Exception {
         final var userSession = mock(UserSession.class);
         final var requestWrapper = new RequestWrapper(null, UUID.randomUUID().toString(), null);
+        final var amqpMessage = new AmqpMessage(Buffer.buffer(objectMapper.writeValueAsBytes(requestWrapper.payload())), null,
+                requestWrapper.event());
+        when(userSession.isValid()).thenReturn(true);
+        when(userSession.createBackendRequest(requestWrapper)).thenReturn(amqpMessage);
 
         messageHandler.handle(userSession, requestWrapper);
 
-        verify(backendService).sendIrisEventToBackend(userSession, requestWrapper.clientTraceId(), requestWrapper);
+        verify(backendService).sendToBackend(requestWrapper.event(), amqpMessage);
     }
 }
