1449e7cbb094c6658824d0fe1f32a6eefbd83551
Tomaz Cerar <tomaz.cerar@gmail.com>
2023-11-07 11:58:49 +0100

    debug jwt token parsing

--- src/main/java/org/iris_events/router/client/AuthClient.java
+++ src/main/java/org/iris_events/router/client/AuthClient.java
@@ -1,13 +1,14 @@
 package org.iris_events.router.client;
 
-import org.eclipse.microprofile.jwt.JsonWebToken;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.smallrye.jwt.auth.principal.JWTParser;
 import io.smallrye.jwt.auth.principal.ParseException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 @ApplicationScoped
 public class AuthClient {
@@ -17,11 +18,19 @@ public class AuthClient {
     JWTParser jwtParser;
 
     public JsonWebToken checkToken(String jwtToken) {
-
+        MDC.put("token", jwtToken);
         try {
             return jwtParser.parse(jwtToken);
         } catch (ParseException e) {
+
+            log.info("token: {}", jwtToken);
+            if (e.getCause() instanceof InvalidJwtException ije) {
+                log.info("invalid jwt, context: {}, details: {}", ije.getJwtContext(),ije.getErrorDetails());
+            }
             log.warn("Could not parse token", e);
+
+        } finally {
+            MDC.remove("token");
         }
         return null;
     }
